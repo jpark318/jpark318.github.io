bb7542595c03a57f147455c22a94f0040df4a1f2
Junbin Park <junbin.park@gmail.com>
2017-12-13 22:39:34 -0600

    personal webpage

---
+++ js/afterpage-script.js
@@ -0,0 +1,22 @@
+// When the user clicks anywhere outside of the modal, close it
+var modal = document.getElementById('ticketModal');
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
+
+function myMap() {
+  myCenter=new google.maps.LatLng(40.101952, -88.227161);
+  var mapOptions= {
+    center:myCenter,
+    zoom:12, scrollwheel: false, draggable: false,
+    mapTypeId:google.maps.MapTypeId.ROADMAP
+  };
+  var map=new google.maps.Map(document.getElementById("googleMap"),mapOptions);
+
+  var marker = new google.maps.Marker({
+    position: myCenter,
+  });
+  marker.setMap(map);
+}
